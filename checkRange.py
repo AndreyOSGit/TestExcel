717d28e2eb5940e18b03447b615d2dca8c972712
unknown <osetsky.ay@gmail.com>
2019-02-11 01:13:10 +0200

    Initial commit

---
+++ checkRange.py
@@ -0,0 +1,24 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['http://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('C:/Users/Andrew/Downloads/GoogleSheetsPG-0eba919971ef.json',
+                                                         scope)
+client = gspread.authorize(creds)
+sheet = client.open('test_py').sheet1
+
+print(sheet.get_all_records())
+
+cell_number_with_C = 4
+cell_check = 2
+row_check = 1
+cell_new = 5
+col_with_c_len = sheet.col_values(cell_number_with_C).__len__() + 1
+cshki = 'C'
+
+for i in range(1, col_with_c_len):
+    if sheet.cell(i, cell_number_with_C).value.__contains__('C') or sheet.cell(i,
+                                                                               cell_number_with_C).value.__contains__(
+            'c'):
+        if sheet.cell(i, cell_check).value == '':
+            sheet.update_cell(i, cell_new, 'Покрыть проверки: ' + cshki)
---
+++ main.py
@@ -0,0 +1,5 @@
+def near_hundred(n):
+  if (210 >= n >= 190) or (110 >= n >= 90) :
+    return True
+  else:
+    return False
