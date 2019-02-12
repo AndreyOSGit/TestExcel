a585ea47b7216995cbc86b8ffb8a132c43629d8c
Andrew <osetsky.ay@gmail.com>
2019-02-12 03:52:33 +0200

    added new copy

--- checkRange.py
+++ checkRange.py
@@ -7,18 +7,21 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('C:/Users/Andrew/Downlo
 client = gspread.authorize(creds)
 sheet = client.open('test_py').sheet1
 
-print(sheet.get_all_records())
+# print(sheet.get_all_records())
 
-cell_number_with_C = 4
-cell_check = 2
+cell_number_with_C = 8
+cell_check = 9
 row_check = 1
-cell_new = 5
+cell_new = 10
 col_with_c_len = sheet.col_values(cell_number_with_C).__len__() + 1
 cshki = 'C'
+CforTasks = []
+thiset = {}
+
+print(sheet.col_values(1).sort()+'\n')
 
 for i in range(1, col_with_c_len):
-    if sheet.cell(i, cell_number_with_C).value.__contains__('C') or sheet.cell(i,
-                                                                               cell_number_with_C).value.__contains__(
-            'c'):
-        if sheet.cell(i, cell_check).value == '':
-            sheet.update_cell(i, cell_new, 'Покрыть проверки: ' + cshki)
+    if (sheet.cell(i, cell_number_with_C).value.__contains__('C') or sheet.cell(i, cell_number_with_C).value.__contains__('c') ) and sheet.cell(i, cell_check).value.__contains__('Task'):
+                while sheet.cell(i, cell_check).value == sheet.cell(i+1, cell_check).value:
+                    cshki =+ sheet.cell(i, cell_number_with_C).value
+                    sheet.update_cell(i, cell_new, 'Покрыть проверки: ' + cshki)
--- createCcover.py
+++ createCcover.py
@@ -2,21 +2,46 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['http://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('C:/Users/Andrew/Downloads/GoogleSheetsPG-0eba919971ef.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('C:/Users/Andrew/Downloads/GoogleSheetsPG-0eba919971ef.json',
+                                                         scope)
 client = gspread.authorize(creds)
 sheet = client.open('test_py').sheet1
 
-print(sheet.get_all_records())
+# print(sheet.get_all_records())
 
-cell_number_with_C = 4
-cell_check = 2
+cell_number_with_C = 8
+cell_check = 9
 row_check = 1
-cell_new = 5
-col_with_c_len = sheet.col_values(cell_number_with_C).__len__()+1
-cshki = 'C'
-
-for i in range(1,col_with_c_len):
-    if sheet.cell(i, cell_number_with_C).value.__contains__('C') or sheet.cell(i, cell_number_with_C).value.__contains__('c'):
-        if sheet.cell(i, cell_check).value == '':
-            sheet.update_cell(i,cell_new,'Покрыть проверки: ' + cshki )
-            
+cell_new = 10
+col_with_c_len = sheet.col_values(cell_number_with_C).__len__() + 1
+cshki = ''
+CforTasks = sheet.col_values(cell_number_with_C)
+print(CforTasks)
+taskN = [int(k) for k in sheet.col_values(cell_check)]
+taskNset = list(set(taskN))
+print(taskN)
+print(taskNset)
+listCover = []
+for i in range(0, CforTasks.__len__()):
+        for j in range(0, taskNset.__len__()):
+            for p in range(0, CforTasks.__len__()):
+                if taskN[p] == taskNset[j]:
+                    cshki = cshki + ', ' + CforTasks[p]
+                # print(cshki)
+                # print(CforTasks)
+            listCover.append(cshki)
+            cshki = ''
+print(listCover)
+a = list(set(listCover))
+out = []
+print('a')
+print(a)
+# sheet.cell(i, cell_number_with_C).value)
+for i in range(0,col_with_c_len-1):
+    for x in a:
+        if (x.__contains__ (CforTasks[i])):
+            out.append('Покрыть проверки: ' + x)
+print('out')
+print(out)
+# for i in range(1,col_with_c_len):
+#     sheet.update_cell(i, cell_new, out[i])
